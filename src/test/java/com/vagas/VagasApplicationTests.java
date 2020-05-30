e1ea072c0d823ea9fae31c5bc6238d97ea2777eb
Lucas_barros_AVA <lucas.barros@damasio.edu.br>
2020-05-30 18:37:16 -0300

    Geração do Projeto

---
+++ src/test/java/com/vagas/VagasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
